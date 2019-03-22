bc4e92265dee43b850f60adbcab54de498331532
kvahid <vahid.karimi89@gmail.com>
2019-03-22 17:22:13 +0430

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+MIT OCW Course 6.0001
+
+Problem SET 0
+
+Developer: Vahid Karimi
+
+Problem Description:
+    
+   Write a program that does the following in order: 
+1. Asks the user to enter a number “x”
+2. Asks the user to enter a number “y”  
+3. Prints out number “x”, raised to the power “y”. 
+4. Prints out the log (base 2) of “x”.  
+"""
+#importing log function from math library
+from math import log
+
+#asking the user to enter two numbers for x and y and changing the numbers to float type
+x = float(input("Enter a number: "))
+y = float(input("Enter another number: "))
+
+#creating the output strings
+s1 = repr(x) + ' raised to the power of ' + repr(y) + ' = ' + repr(x**y)
+s2 = 'log (base 2) of ' + repr(x) + ' = ' + repr(log(x, 2))
+
+#printing out the outputs
+print('\n')
+print(s1)
+print(s2)
